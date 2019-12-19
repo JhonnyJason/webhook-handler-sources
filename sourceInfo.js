85117d3b48e76a92e53156bef81515be48c800c5
Lenard Frommelt <lenny09918050@googlemail.com>
2019-12-19 02:59:29 +0100

    updated and upgraded tependencies

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,8 +1,8 @@
 module.exports = {
     getDependencies: () => {
         return {
-            "body-parser": "^1.18.3",
-            "express": "^4.16.3",
+            "body-parser": "^1.19.0",
+            "express": "^4.17.1",
             "express-github-webhook": "^1.0.6",
             "systemd": "^0.3.1"
         }
